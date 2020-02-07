905ffdf82de1254a7d48759e5da9c42a91e441f6
johntahod <johntahod@gmail.com>
2020-02-06 22:02:32 -0800

    changes made to prep for Heading

--- CubeSat/ComputerVision/testLanding.py
+++ CubeSat/ComputerVision/testLanding.py
@@ -17,6 +17,18 @@ from opencv.arucotracklib import *
 #--------------------------------------------------    
 
 
+#Goal of this navigation is to have CubeSat Rotate a degree/ angle; with the goal of 
+# having the target aligned in its center before thrusting toward target.
+
+# ---HEADING CONTROLS
+# By knowing how long SimPlat takes to rotate a full 360 we can computer the amount of
+# time it takes to move N degrees. 
+# Now that we have time it takes to rotate a certain degree/angle, we can use this to send a 
+# rotate command for X amount of seconds before sending counter thrusting to stop rotation .
+# This will line us up with the target in the center of our screen. 
+
+# XYZ -> Angle-> Rotate(angle) ->computeRotateTime(angel) ->startRotate(seconds )
+
 
 def marker_position_to_angle(x, y, z):
     
@@ -25,22 +37,9 @@ def marker_position_to_angle(x, y, z):
     
     return (angle_x, angle_y)
     
-def camera_to_uav(x_cam, y_cam):
-    x_uav =-y_cam
-    y_uav = x_cam
-    return(x_uav, y_uav)
-    
-def uav_to_ne(x_uav, y_uav, yaw_rad):
-    c       = math.cos(yaw_rad)
-    s       = math.sin(yaw_rad)
-    
-    north   = x_uav*c - y_uav*s
-    east    = x_uav*s + y_uav*c 
-    return(north, east)
-    
-def check_angle_descend(angle_x, angle_y, angle_desc):
-    return(math.sqrt(angle_x**2 + angle_y**2) <= angle_desc)
-        
+
+   
+       
 #--------------------------------------------------
 #-------------- CONNECTION  
 #--------------------------------------------------    
@@ -55,10 +54,8 @@ print('Connecting...')
 #--- Define Tag
 id_to_find      = 24
 marker_size     = 10 #- [cm]
-freq_send       = 1 #- Hz
 
-land_alt_cm         = 50.0
-land_speed_cms      = 30.0
+distanceGoal        = 50.0
 
 
 
@@ -66,8 +63,8 @@ land_speed_cms      = 30.0
 # Find full directory path of this script, used for loading config and other files
 cwd                 = path.dirname(path.abspath(__file__))
 calib_path          = cwd+"/../opencv/"
-camera_matrix       = np.loadtxt(calib_path+'cameraMatrix_raspi.txt', delimiter=',')
-camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_raspi.txt', delimiter=',')                                      
+camera_matrix       = np.loadtxt('cameraMatrix_raspi.txt', delimiter=',')
+camera_distortion   = np.loadtxt('cameraDistortion_raspi.txt', delimiter=',')                                      
 aruco_tracker       = ArucoSingleTracker(id_to_find=id_to_find, marker_size=marker_size, show_video=False, 
                 camera_matrix=camera_matrix, camera_distortion=camera_distortion)
                 
@@ -76,20 +73,25 @@ time_0 = time.time()
 
 while True:                
 
-    marker_found, x_cm, y_cm, z_cm = aruco_tracker.track(loop=False)
-    if marker_found:
-        x_cm, y_cm          = camera_to_uav(x_cm, y_cm)
-        
-        angle_x, angle_y    = marker_position_to_angle(x_cm, y_cm, z_cm)
+    marker_found, x, y, z = aruco_tracker.track(loop=False) # Note : XYZ  are all in cm
 
-        print("X ; ",x_cm)
-        print("Y ; ",y_cm)
-        print("Z ; ",z_cm)
+    if marker_found:
+        
+        angle_x, angle_y    = marker_position_to_angle(x, y, z)
+
+        print("X ; ",x)
+        print("Y ; ",y)
+        print("Z ; ",z)
         print("angleX : ",angle_x)
         print("angleY : ",angle_y)
 
 
         #--- COmmand to land
-        if z_cm <= land_alt_cm:
-             print (" -->>COMMANDING TO LAND<<")
+        if z <= distanceGoal:
+             print (" -->>Target Distination Reached <<")
+    if marker_found is False:
+        print("X: 0")
+        print("Y: 0")
+        print("Z: 0")
+        print("rotate until find !")
             
