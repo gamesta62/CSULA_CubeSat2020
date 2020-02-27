9fe04a0fdb90ec5c491787da8b1bd285b0bff214
johntahod <johntahod@gmail.com>
2020-02-26 20:56:47 -0800

    aruco tracking using timer, to prevent flooding of data

--- CubeSat/ComputerVision/arucotrack.py
+++ CubeSat/ComputerVision/arucotrack.py
@@ -49,34 +49,6 @@ class ArucoTracker():
         self.fps_read    = 0.0
         self.fps_detect  = 0.0 
 
-    # def _rotationMatrixToEulerAngles(self,R):
-    # # Calculates rotation matrix to euler angles
-    # # The result is the same as MATLAB except the order
-    # # of the euler angles ( x and z are swapped ).
-    
-    #     def isRotationMatrix(R):
-    #         Rt = np.transpose(R)
-    #         shouldBeIdentity = np.dot(Rt, R)
-    #         I = np.identity(3, dtype=R.dtype)
-    #         n = np.linalg.norm(I - shouldBeIdentity)
-    #         return n < 1e-6        
-    #     assert (isRotationMatrix(R))
-
-    #     sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-
-    #     singular = sy < 1e-6
-
-    #     if not singular:
-    #         x = math.atan2(R[2, 1], R[2, 2])
-    #         y = math.atan2(-R[2, 0], sy)
-    #         z = math.atan2(R[1, 0], R[0, 0])
-    #     else:
-    #         x = math.atan2(-R[1, 2], R[1, 1])
-    #         y = math.atan2(-R[2, 0], sy)
-    #         z = 0
-
-    #     return np.array([x, y, z])
-
     def _update_fps_read(self):
         t           = time.time()
         self.fps_read    = 1.0/(t - self._t_read)
@@ -145,11 +117,6 @@ class ArucoTracker():
                 R_ct    = np.matrix(cv2.Rodrigues(rvec)[0])
                 R_tc    = R_ct.T
 
-                #-- Get the attitude in terms of euler 321 (Needs to be flipped first)
-                # roll_marker, pitch_marker, yaw_marker = self._rotationMatrixToEulerAngles(self._R_flip*R_tc)
-
-            
-
                 #-- Now get Position and attitude f the camera respect to the marker
                 pos_camera = -R_tc*np.matrix(tvec).T
                 # print( "Camera X = %4.0f  Y = %4.0f  Z = %4.0f ",(pos_camera[0], pos_camera[1], pos_camera[2]))
@@ -167,20 +134,11 @@ class ArucoTracker():
                     str_position = "MARKER Position x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
                     cv2.putText(frame, str_position, (0, 100), font, 1, (0, 255, 0), 2, cv2.LINE_AA)        
                     
-                    #-- Print the marker's attitude respect to camera frame
-                    # str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),math.degrees(pitch_marker),
-                    #                     math.degrees(yaw_marker))
-                    # cv2.putText(frame, str_attitude, (0, 150), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
+               
                     str_position = "CAMERA Position x=%4.0f  y=%4.0f  z=%4.0f"%(pos_camera[0], pos_camera[1], pos_camera[2])
                     cv2.putText(frame, str_position, (0, 200), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-                    #-- Get the attitude of the camera respect to the frame
-                    # roll_camera, pitch_camera, yaw_camera = self._rotationMatrixToEulerAngles(self._R_flip*R_tc)
-
-                    # str_attitude = "CAMERA Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_camera),math.degrees(pitch_camera),
-                    #                     math.degrees(yaw_camera))
-                    # cv2.putText(frame, str_attitude, (0, 250), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                
 
             else:
                 # if nothing found keep searching , 
@@ -218,11 +176,12 @@ if __name__ == "__main__":
     camera_matrix   = np.loadtxt(calib_path+'cameraMatrix_raspi.txt', delimiter=',')
     camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_raspi.txt', delimiter=',')                                      
     aruco_tracker = ArucoTracker(id_to_find=24, marker_size=10, show_video=True, camera_matrix=camera_matrix, camera_distortion=camera_distortion)
+
+
+    
     a = aruco_tracker.track(info=True)
 
-while True :    
-    print (a)
-    
+
     
 
 
